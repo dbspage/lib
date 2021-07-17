7eef7eef213813f47566e1f5cc0649ee7fa4e1c5
dbspage <74287959+dbspage@users.noreply.github.com>
2021-07-17 15:52:06 -0500

    Add files via upload

---
+++ rst.js
@@ -0,0 +1,4 @@
+function lfd(x){
+var lfd= x;
+document.write('<span class="element4" onclick="element2(&apos;element3&apos;)"><img border="0" src="https://1.bp.blogspot.com/-eE0lBImxssc/YIX2NjAj79I/AAAAAAAACTY/UJ6UsbRB10Afx8Xb9DydPkLSaJOJ-MJUwCNcBGAsYHQ/s40/klipar'+lfd+'.com.png" style="position: absolute;z-index: 300;margin-left: -100px;margin-top: 50px;border: 0px;"/></span>');
+}
